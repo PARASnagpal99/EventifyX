b7dab7fe66053e39b3b96df1334467af670138bf
PARASnagpal99 <paras.nagpal743@gmail.com>
2024-01-19 10:29:36 +0530

    Solved CORS issue

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const connectDB = require('./config/db'); 
+const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 const eventRoutes = require('./routes/eventRoutes')
 
@@ -13,13 +14,7 @@ const app = express();
 // Middlewares 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const corsOptions = {
-    origin: 'http://localhost:3000', // Allow requests from this origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Allow cookies to be sent with the requests
-    optionsSuccessStatus: 204,
-  };
-app.use(cors(corsOptions));
+app.use(cors());
 
 // Routes 
 app.use('/api/v1/user',userRoutes);
