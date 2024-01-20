1b21e26c415ff1fd785e7eb173de50128596c2db
Ritesh512 <riteshprajapati1359@gmail.com>
2024-01-20 12:23:30 +0530

    Add cancelRegistration remove interest

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,16 +7,20 @@ const {
   registerUserForEvent,
   userRegisteredEvents,
   deleteUser ,
-  getUserInterests
+  getUserInterests,
+  removeUserInterest,
+  unregisterUserForEvent
 } = require("../controllers/userControllers");
 
 router.route("/signup").post(signup);
 router.route("/login").post(login);
 router.route("/addInterest/:userId").put(addUserInterest);
+router.route("/removeInterest/:userId").put(removeUserInterest);
 router.route("/userInterest/:userId").get(getUserInterests);
 router.route("/registerEvent/:userId").post(registerUserForEvent);
 router.route("/deleteUser/:userId").delete(deleteUser);
 router.route("/userRegisteredEvents/:userId").get(userRegisteredEvents);
+router.route("/cancelUserRegistration/:userId").delete(unregisterUserForEvent);
 router.route("/userInterest/:userId").get(getUserInterests);
 
 
